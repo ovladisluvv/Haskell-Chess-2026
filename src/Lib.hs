93fd5f7d4c356835d66902016ba9744d46695018
Огай Владислав <ogaivl@my.msu.ru>
2026-04-02 04:03:32 +0300

    add Windows release workflow

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,13 +2,16 @@ module Lib
     ( run
     ) where
 
-import Graphics.Gloss 
+import Bot (isBotTurn, makeBotMove)
 import Board (initGameState)
+import Events (handleEvent)
+import Graphics.Gloss
+import Render (drawGame, windowHeight, windowWidth)
+import System.Directory (doesFileExist)
+import System.Environment (getExecutablePath)
+import System.FilePath ((</>), takeDirectory)
 import Types (GameState(..), Piece(..), PieceType(..))
 import qualified Types as T
-import Render (windowWidth, windowHeight, drawGame)
-import Events (handleEvent)
-import Bot (isBotTurn, makeBotMove)
 
 -- Главная функция запуска приложения
 -- Окно приложения
@@ -27,36 +30,49 @@ fps = 30
 update :: Float -> GameState -> GameState
 update dt state
     -- Если сейчас ход бота, делаем ход после задержки
-    | isBotTurn state = 
+    | isBotTurn state =
         if botTimer state <= 0
         then makeBotMove state
         else state { botTimer = botTimer state - dt }
     | otherwise = state
 
+resolveAssetPath :: FilePath -> IO FilePath
+resolveAssetPath fileName = do
+    exePath <- getExecutablePath
+    let packagedPath = takeDirectory exePath </> "assets" </> "pieces" </> fileName
+        devPath = "assets" </> "pieces" </> fileName
+    packagedExists <- doesFileExist packagedPath
+    pure $ if packagedExists then packagedPath else devPath
+
+loadPieceImage :: FilePath -> IO Picture
+loadPieceImage fileName = do
+    path <- resolveAssetPath fileName
+    loadBMP path
+
 -- Загружаем спрайты
 loadImages :: IO [(Piece, Picture)]
 loadImages = do
-    wpPic <- loadBMP "assets/pieces/wp.bmp"
-    bpPic <- loadBMP "assets/pieces/bp.bmp"
-    wnPic <- loadBMP "assets/pieces/wn.bmp"
-    bnPic <- loadBMP "assets/pieces/bn.bmp"
-    wkPic <- loadBMP "assets/pieces/wk.bmp"
-    bkPic <- loadBMP "assets/pieces/bk.bmp"
-    wbPic <- loadBMP "assets/pieces/wb.bmp"
-    bbPic <- loadBMP "assets/pieces/bb.bmp"
-    wrPic <- loadBMP "assets/pieces/wr.bmp"
-    brPic <- loadBMP "assets/pieces/br.bmp"
-    wqPic <- loadBMP "assets/pieces/wq.bmp"
-    bqPic <- loadBMP "assets/pieces/bq.bmp"
+    wpPic <- loadPieceImage "wp.bmp"
+    bpPic <- loadPieceImage "bp.bmp"
+    wnPic <- loadPieceImage "wn.bmp"
+    bnPic <- loadPieceImage "bn.bmp"
+    wkPic <- loadPieceImage "wk.bmp"
+    bkPic <- loadPieceImage "bk.bmp"
+    wbPic <- loadPieceImage "wb.bmp"
+    bbPic <- loadPieceImage "bb.bmp"
+    wrPic <- loadPieceImage "wr.bmp"
+    brPic <- loadPieceImage "br.bmp"
+    wqPic <- loadPieceImage "wq.bmp"
+    bqPic <- loadPieceImage "bq.bmp"
 
-    return [ 
-        (Piece Pawn T.White, wpPic),   (Piece Pawn T.Black, bpPic),
-        (Piece Knight T.White, wnPic), (Piece Knight T.Black, bnPic),
-        (Piece King T.White, wkPic),   (Piece King T.Black, bkPic),
-        (Piece Bishop T.White, wbPic), (Piece Bishop T.Black, bbPic),
-        (Piece Rook T.White, wrPic),   (Piece Rook T.Black, brPic),
-        (Piece Queen T.White, wqPic),  (Piece Queen T.Black, bqPic) 
-      ]
+    return
+        [ (Piece Pawn T.White, wpPic),   (Piece Pawn T.Black, bpPic)
+        , (Piece Knight T.White, wnPic), (Piece Knight T.Black, bnPic)
+        , (Piece King T.White, wkPic),   (Piece King T.Black, bkPic)
+        , (Piece Bishop T.White, wbPic), (Piece Bishop T.Black, bbPic)
+        , (Piece Rook T.White, wrPic),   (Piece Rook T.Black, brPic)
+        , (Piece Queen T.White, wqPic),  (Piece Queen T.Black, bqPic)
+        ]
 
 -- Запуск
 run :: IO ()
